ea8d16434c3ad382bd1881ac94a1d839701571df
Karan@02 <krnptl1234@gmail.com>
2022-08-10 20:37:40 +0530

    fifth

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3010;
+const publicPath = path.join(__dirname, 'build');
+console.log(publicPath);
+const port = process.env.PORT || 3009;
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
